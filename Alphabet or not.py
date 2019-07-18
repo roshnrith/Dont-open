2f401c89500ef428cef62954754ed352517d78fe
roshnrith <53034624+roshnrith@users.noreply.github.com>
2019-07-18 14:17:47 +0530

    Create Alphabet or not.py

---
+++ Alphabet or not.py
@@ -0,0 +1,7 @@
+A=(input(""))
+if ((A =='1' or A =='2' or A =='3' or A =='4' or A =='5' or A =='6' or A =='7' or A =='8' or A =='9' or A =='0' )): 
+    print("No") 
+else:
+    print ("Alphabet")
+
+    
